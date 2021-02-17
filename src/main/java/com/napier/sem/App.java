2d51eab1ec01ff4aa643ba8503102c6eb2fa98c4
Cameron Hunt <cameronhunt88@hotmail.com>
2021-02-17 11:00:37 +0000

    Commented out print all employees to stop travis erroring

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -31,7 +31,7 @@ public class App
         // Test the size of the returned data - should be 240124
         System.out.println(employees.size());
         // Print out all employee salaries
-        a.printSalaries(employees);
+        //a.printSalaries(employees);
 
         // Disconnect from Database
         a.disconnect();
