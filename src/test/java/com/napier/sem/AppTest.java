4cce07260d178b64ff97b0324d0a0fb3658600cb
Cameron Hunt <cameronhunt88@hotmail.com>
2021-03-08 14:13:28 +0000

    UnitTests, currently 5/6 pass

---
+++ src/test/java/com/napier/sem/AppTest.java
@@ -0,0 +1,85 @@
+package com.napier.sem;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest
+{
+    static App app;
+
+    @BeforeAll
+    static void init()
+    {
+        app = new App();
+    }
+
+    // Print Salaries Unit Tests
+    @Test
+    void printSalariesTestNull()
+    {
+        app.printSalaries(null);
+    }
+
+    @Test
+    void printSalariesTestContainsNull()
+    {
+        ArrayList<Employee> employees = new ArrayList<>();
+        employees.add(null);
+        app.printSalaries(employees);
+    }
+
+    @Test
+    void printSalaries()
+    {
+        ArrayList<Employee> employees = new ArrayList<>();
+        Employee emp = new Employee();
+        emp.emp_no = 1;
+        emp.first_name = "Cameron";
+        emp.last_name = "Hunt";
+        emp.title = "Engineer";
+        emp.salary = 75000;
+        employees.add(emp);
+        app.printSalaries(employees);
+    }
+
+    // Display Employee Unit Tests
+    @Test
+    void displayEmployeeTestNull()
+    {
+        app.displayEmployee(null);
+    }
+
+    @Test
+    void displayEmployeeTestContainsNull()
+    {
+        Employee emp;
+        emp = null;
+        app.displayEmployee(emp);
+    }
+
+    @Test
+    void displayEmployee()
+    {
+        Department dept = new Department();
+        dept.dept_name = "Sales";
+        Employee manager = new Employee();
+        manager.first_name = "Daniel";
+        manager.last_name = "Test";
+        Employee emp = new Employee();
+        emp.emp_no = 1;
+        emp.first_name = "Cameron";
+        emp.last_name = "Hunt";
+        emp.title = "Engineer";
+        emp.salary = 85000;
+        emp.dept.dept_name = dept.dept_name;
+        emp.manager.first_name = manager.first_name;
+        emp.manager.last_name = manager.last_name;
+
+        app.displayEmployee(emp);
+    }
+}
